1e9f01ee9f955a69b4403925654ea91d0f751548
gautamamber <ambergautam1@gmail.com>
2020-05-24 23:47:14 +0530

    feat():add bucket serializer

---
+++ bucketapp/serializers.py
@@ -0,0 +1,10 @@
+from rest_framework import serializers
+from . import models
+
+
+class BucketListSerializer(serializers.ModelSerializer):
+    """Serializer to map the model instance into JSON"""
+
+    class Meta:
+        model = models.BucketList
+        fields = ('name',)
